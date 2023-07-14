375152db8364f0314ad7d2fab70d4214fe780a12
Pete <88169944+Xio1996@users.noreply.github.com>
2023-07-14 10:36:11 +0100

    Update FOVI Rotator.js
    Uses Rotation property of the Sky Chart.

--- Scripts/FOVI Rotator.js
+++ Scripts/FOVI Rotator.js
@@ -2,27 +2,16 @@
 /* Socket Start Packet */
 var Out;
 /* Find centre of chart and calculate PA for rotator */
-try
-{
-	if (ccdsoftCamera.rotatorIsConnected() == 0)
-	{
+try {
+	if (ccdsoftCamera.rotatorIsConnected() == 0) {
 		ccdsoftCamera.rotatorConnect();
 		Out = "Rotator Connected,"
 	}
 
-	if (ccdsoftCamera.rotatorIsConnected() > 0)
-	{
-		var cRA = sky6StarChart.RightAscension;
-		var cDec = sky6StarChart.Declination;
-
-		sky6Utils.ConvertRADecToAzAlt(cRA, cDec)
-		sky6Utils.ConvertAzAltToRADec(sky6Utils.dOut0, sky6Utils.dOut1 - 0.25)
-
-		sky6Utils.ComputePositionAngle(cRA, cDec, sky6Utils.dOut0, sky6Utils.dOut1)
-
-		ccdsoftCamera.rotatorGotoPositionAngle(sky6Utils.dOut0);
-
-		Out = Out + " RA:" + cRA + " Dec:" + cDec + " PA:" + sky6Utils.dOut0;
+	if (ccdsoftCamera.rotatorIsConnected() > 0) {
+		var Angle = sky6StarChart.Rotation
+		ccdsoftCamera.rotatorGotoPositionAngle(Angle);
+		Out = "  PA:" + Angle.toFixed(3);
 	}
 	else {
 		Out = "-2";
